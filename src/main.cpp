ccd1b16b3df0ad14c8411473f4e1627b0bc4f2c0
necobit <dm@necobit.com>
2025-10-25 09:02:31 +0900

    pinMode追加

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,14 @@ constexpr int PIN_SERIAL2_TX = 4; // Serial2 TX
 
 void setup()
 {
+  delay(3000);
   auto cfg = M5.config();
   M5.begin(cfg);
   delay(500);
   Serial.begin(115200);
   delay(500);
+  pinMode(PIN_SERIAL2_TX, INPUT_PULLUP);
+  pinMode(PIN_SERIAL2_RX, INPUT_PULLUP);
   Serial2.begin(9600, SERIAL_8N1, PIN_SERIAL2_RX, PIN_SERIAL2_TX);
   delay(500);
 }
